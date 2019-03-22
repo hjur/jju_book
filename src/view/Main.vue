9dfe7418c901bb784ce545ad2f88470aedc12fb7
황주리 <hjur91@interpark.com>
2019-03-22 19:00:23 +0900

    라우터 커밋밋

---
+++ src/view/Main.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'PageNotFound',
+  computed: {},
+  components: {
+  },
+  data() {
+    return {
+      msg: 'jju book 메인',
+    }
+  }
+}
+// import "SharedStyles/popup/popup.css";
+</script>
+
+<style>
+</style>
