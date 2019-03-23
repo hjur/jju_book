4e2d35e635290688e5c155739a004c425b9747d4
hjur <hjurzz@gmail.com>
2019-03-23 22:28:55 +0900

    라우터 이동 테스트

--- src/config/route.js
+++ src/config/route.js
@@ -2,15 +2,21 @@ import VueRouter from 'vue-router'
 
 //메인
 import Main from 'View/Main.vue';
+//ETC 카테고리
+import ETC from 'View/ETC/EtcMain.vue';
 //에러페이지
 import PageNotFound from 'View/PageNotFound.vue';
 
 //루트디렉토리
 const root = '/';
 const routes = [{
-    name: 'main',
+    name: 'mainPage',
     path: root.concat('/'),
     component: Main,
+  },{
+    name: 'etc',
+    path: root.concat('/etc'),
+    component: ETC,
   }]
   //404가 가장 마지막에 위치해야함
   .concat({
---
+++ src/view/ETC/EtcMain.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'etcMain',
+  computed: {},
+  components: {
+  },
+  data() {
+    return {
+      msg: 'etc 메인',
+    }
+  }
+}
+// import "SharedStyles/popup/popup.css";
+</script>
+
+<style>
+</style>
